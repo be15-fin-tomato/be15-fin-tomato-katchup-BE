dba71d9c3a642cb6ca5cf134c98593210bf35475
kkkwid <lsj9057@daum.net>
2025-06-12 18:46:09 +0900

    commit

---
+++ src/main/java/be15fintomatokatchupbe/Be15FinTomatoKatchupBeApplication.java
@@ -0,0 +1,13 @@
+package be15fintomatokatchupbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be15FinTomatoKatchupBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be15FinTomatoKatchupBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be15fintomatokatchupbe/Be15FinTomatoKatchupBeApplicationTests.java
@@ -0,0 +1,13 @@
+package be15fintomatokatchupbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be15FinTomatoKatchupBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
