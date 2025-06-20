ba8912ce64e5129889c4d4399f955627326ef0ce
Joonspar <sdwang15@naver.com>
2025-06-20 16:28:55 +0900

    feat: 인플루언서 관리 수정 , 삭제 기능 구현

--- src/main/java/be15fintomatokatchupbe/influencer/command/domain/repository/HashtagInfluencerCampaignRepository.java
+++ src/main/java/be15fintomatokatchupbe/influencer/command/domain/repository/HashtagInfluencerCampaignRepository.java
@@ -2,9 +2,16 @@ package be15fintomatokatchupbe.influencer.command.domain.repository;
 
 import be15fintomatokatchupbe.relation.domain.HashtagInfluencerCampaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface HashtagInfluencerCampaignRepository extends JpaRepository<HashtagInfluencerCampaign, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM HashtagInfluencerCampaign h WHERE h.influencerId = :influencerId")
     void deleteByInfluencerId(Long influencerId);
 }
