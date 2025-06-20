cebd6423b227f3bbf8c0e85c097e5ff85b99b9d5
Cho-Hyun-Seung <toki0327@naver.com>
2025-06-20 10:09:13 +0900

    chore: 컨트롤러, 서비스 생성

--- src/main/java/be15fintomatokatchupbe/campaign/command/application/controller/CampaignCommandController.java
+++ src/main/java/be15fintomatokatchupbe/campaign/command/application/controller/CampaignCommandController.java
@@ -1,8 +1,10 @@
 package be15fintomatokatchupbe.campaign.command.application.controller;
 
 import be15fintomatokatchupbe.campaign.command.application.dto.request.CreateChanceRequest;
+import be15fintomatokatchupbe.campaign.command.application.service.CampaignCommandService;
 import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/chance")
 public class CampaignCommandController {
+
+    private final CampaignCommandService campaignCommandService;
 
     public ResponseEntity<ApiResponse<Void>> createChance(
             @AuthenticationPrincipal CustomUserDetail user,
@@ -19,7 +24,7 @@ public class CampaignCommandController {
     ){
         Long userId =user.getUserId();
 
-
+        campaignCommandService.createChance(userId, request);
 
         return ResponseEntity.ok(ApiResponse.success(null));
 
--- src/main/java/be15fintomatokatchupbe/campaign/command/application/service/CampaignCommandService.java
+++ src/main/java/be15fintomatokatchupbe/campaign/command/application/service/CampaignCommandService.java
@@ -1,4 +1,11 @@
 package be15fintomatokatchupbe.campaign.command.application.service;
 
+import be15fintomatokatchupbe.campaign.command.application.dto.request.CreateChanceRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CampaignCommandService {
+    public void createChance(Long userId, CreateChanceRequest request) {
+
+    }
 }
