306efdf84c31261bd01edda9564fed28b827b79a
oyk0510 <youkyungoh0510@naver.com>
2025-06-20 16:12:04 +0900

    refactor: 고객사 참조용 파일 구조, 파일명 리팩토링

--- src/main/java/be15fintomatokatchupbe/campaign/command/application/service/CampaignCommandService.java
+++ src/main/java/be15fintomatokatchupbe/campaign/command/application/service/CampaignCommandService.java
@@ -1,6 +1,5 @@
 package be15fintomatokatchupbe.campaign.command.application.service;
 
-import be15fintomatokatchupbe.client.command.application.service.ClientCommandService;
 import org.springframework.stereotype.Service;
 
 @Service
