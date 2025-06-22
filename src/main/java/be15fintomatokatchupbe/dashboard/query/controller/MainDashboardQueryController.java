a0e7b728195898f3dd3a6f103323b415f9edc16e
lddocy <jkcy911219@naver.com>
2025-06-22 19:02:39 +0900

    refactor: 대시보드 컨트롤러에 swagger 작성

--- src/main/java/be15fintomatokatchupbe/dashboard/query/controller/MainDashboardQueryController.java
+++ src/main/java/be15fintomatokatchupbe/dashboard/query/controller/MainDashboardQueryController.java
@@ -5,6 +5,8 @@ import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
 import be15fintomatokatchupbe.dashboard.query.dto.response.QuotationResponse;
 import be15fintomatokatchupbe.dashboard.query.dto.response.*;
 import be15fintomatokatchupbe.dashboard.query.service.MainDashboardQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "대시보드")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dashboard")
@@ -21,6 +24,7 @@ public class MainDashboardQueryController {
 
     private final MainDashboardQueryService mainDashboardQueryService;
 
+    @Operation(summary = "신규 영업활동 조회", description = "사용자는 메인 대시보드에서 최근 30일 이내의 신규 영업활동 개수를 조회할 수 있다.")
     @GetMapping("/sales-activity")
     public ResponseEntity<ApiResponse<SalesActivityResponse>> getSalesActivity(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -28,6 +32,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "담당 고객사 조회", description = "사용자는 메인 대시보드에서 본인이 담당한 고객사의 목록을 조회할 수 있다.")
     @GetMapping("/client-company")
     public ResponseEntity<ApiResponse<List<ClientCompanyResponse>>> getClientCompany(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -35,6 +40,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "오늘 일정 조회", description = "사용자는 메인 대시보드에서 오늘의 일정을 조회할 수 있다.")
     @GetMapping("/schedule")
     public ResponseEntity<ApiResponse<List<TodayScheduleResponse>>> getTodaySchedule(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -42,6 +48,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "담당 리스트업 조회", description = "사용자는 메인 대시보드에서 자신이 담당한 리스트업의 목록을 조회할 수 있다.")
     @GetMapping("/list-up")
     public ResponseEntity<ApiResponse<List<ListupResponse>>> getListup(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -49,6 +56,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "담당 제안 조회", description = "사용자는 메인 대시보드에서 자신이 담당한 제안의 목록을 조회할 수 있다.")
     @GetMapping("/proposal")
     public ResponseEntity<ApiResponse<List<ProposalResponse>>> getProposal(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -56,6 +64,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "담당 계약 조회", description = "사용자는 메인 대시보드에서 자신이 담당한 계약의 목록을 조회할 수 있다.")
     @GetMapping("/contract")
     public ResponseEntity<ApiResponse<List<ContractResponse>>> getContract(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
@@ -63,6 +72,7 @@ public class MainDashboardQueryController {
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
+    @Operation(summary = "담당 견적 조회", description = "사용자는 메인 대시보드에서 자신이 담당한 견적의 목록을 조회할 수 있다.")
     @GetMapping("/quotation")
     public ResponseEntity<ApiResponse<List<QuotationResponse>>> getQuotation(@AuthenticationPrincipal CustomUserDetail user) {
         Long userId = user.getUserId();
--- src/main/java/be15fintomatokatchupbe/dashboard/query/controller/PlatformDashboardQueryController.java
+++ src/main/java/be15fintomatokatchupbe/dashboard/query/controller/PlatformDashboardQueryController.java
@@ -4,6 +4,8 @@ import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
 import be15fintomatokatchupbe.dashboard.query.dto.response.CampaignResponse;
 import be15fintomatokatchupbe.dashboard.query.service.PlatformDashboardQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "대시보드")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/dashboard")
 public class PlatformDashboardQueryController {
     private final PlatformDashboardQueryService platformDashboardQueryService;
 
+    @Operation(summary = "인플루언서 별 캠페인 조회", description = "사용자는 플랫폼 대시보드에서 해당 인플루언서가 진행했던 캠페인의 목록을 조회할 수 있다.")
     @GetMapping("/campaign/{influencerId}")
     public ResponseEntity<ApiResponse<List<CampaignResponse>>> getSalesActivity(
             @AuthenticationPrincipal CustomUserDetail user,
