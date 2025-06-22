4c6998333381a8c0eb8f79a551dee2d0021f70fe
Joonspar <sdwang15@naver.com>
2025-06-23 01:00:50 +0900

    feat: 채팅 나가기 기능 구현

--- src/main/java/be15fintomatokatchupbe/chat/command/application/controller/ChatCommandController.java
+++ src/main/java/be15fintomatokatchupbe/chat/command/application/controller/ChatCommandController.java
@@ -1,17 +1,16 @@
 package be15fintomatokatchupbe.chat.command.application.controller;
 
 import be15fintomatokatchupbe.chat.command.application.dto.request.CreateChatRoomRequest;
+import be15fintomatokatchupbe.chat.command.application.dto.request.ExitChatRoomRequest;
 import be15fintomatokatchupbe.chat.command.application.dto.response.CreateChatRoomResponse;
+import be15fintomatokatchupbe.chat.command.application.dto.response.ExitChatRoomResponse;
 import be15fintomatokatchupbe.chat.command.application.service.ChatCommandService;
 import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
@@ -26,6 +25,15 @@ public class ChatCommandController {
     ){
         request.setUserId(user.getUserId());
         CreateChatRoomResponse response = chatCommandService.createChatRoom(user.getUserId(),request.getUserIds());
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @PatchMapping("/exit")
+    public ResponseEntity<ApiResponse<ExitChatRoomResponse>> exitChat(
+            @RequestBody ExitChatRoomRequest request , @AuthenticationPrincipal CustomUserDetail user
+    ){
+        request.setUserId(user.getUserId());
+        ExitChatRoomResponse response = chatCommandService.exitRoom(user.getUserId(), request.getChatId());
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
--- src/main/java/be15fintomatokatchupbe/chat/command/domain/repository/UserChatRepository.java
+++ src/main/java/be15fintomatokatchupbe/chat/command/domain/repository/UserChatRepository.java
@@ -1,6 +1,18 @@
 package be15fintomatokatchupbe.chat.command.domain.repository;
 
 import be15fintomatokatchupbe.chat.command.domain.aggregate.entity.UserChat;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface UserChatRepository extends JpaRepository<UserChat, Long> {}
+import java.util.Optional;
+
+@Repository
+public interface UserChatRepository extends JpaRepository<UserChat, Long> {
+
+    @Query("SELECT uc FROM UserChat uc WHERE uc.userId = :userId AND uc.chatId = :chatId")
+    Optional<UserChat> findByUserIdAndChatId(@Param("userId") Long userId,
+                                             @Param("chatId") Long chatId);
+
+}
