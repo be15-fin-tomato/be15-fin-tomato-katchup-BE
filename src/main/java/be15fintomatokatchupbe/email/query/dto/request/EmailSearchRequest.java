3fef83579a46c56d108489e166558e02d7a9af34
kkkwid <lsj9057@daum.net>
2025-06-23 14:54:48 +0900

    Feat: 만족도 조사 목록 조회구현

--- src/main/java/be15fintomatokatchupbe/email/query/dto/request/EmailSearchRequest.java
+++ src/main/java/be15fintomatokatchupbe/email/query/dto/request/EmailSearchRequest.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 public class EmailSearchRequest {
 
     private Integer page = 1;
-    private Integer size = 20;
+    private Integer size = 10;
     private String campaignName;
 
     public int getOffset() {
