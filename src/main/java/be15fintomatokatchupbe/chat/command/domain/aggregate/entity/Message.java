5f00736b3283e3303474f36eaf485cf1f6887a6c
Joonspar <sdwang15@naver.com>
2025-06-23 14:17:49 +0900

    feat: 채팅 송수신 기능 구현

---
+++ src/main/java/be15fintomatokatchupbe/chat/command/domain/aggregate/entity/Message.java
@@ -0,0 +1,25 @@
+package be15fintomatokatchupbe.chat.command.domain.aggregate.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message { // MongoDB
+
+    @Id
+    private String id;
+    private Long chatId;
+    private Long senderId;
+    private Long receiverId;
+
+    private String message;
+    private LocalDateTime sentAt;
+}
