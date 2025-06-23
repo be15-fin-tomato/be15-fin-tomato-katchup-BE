5a20b4c4cf330b6c57cf1b40b615f0245b404baa
Joonspar <sdwang15@naver.com>
2025-06-24 00:35:36 +0900

    feat: 전자계약서 템플릿 생성 기능 구현

---
+++ src/main/java/be15fintomatokatchupbe/contract/command/application/dto/response/DigitalContractCreateResponse.java
@@ -0,0 +1,10 @@
+package be15fintomatokatchupbe.contract.command.application.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class DigitalContractCreateResponse {
+    private String message;
+}
