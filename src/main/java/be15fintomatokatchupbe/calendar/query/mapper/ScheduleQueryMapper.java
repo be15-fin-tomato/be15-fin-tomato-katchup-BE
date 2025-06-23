dc04b47a265d1dc27ef2fdb4b508575be474c191
Jangwoo0710 <jangwoo0710@gmail.com>
2025-06-23 14:47:10 +0900

    개인 일정 조회 기능 구현(날짜별&전체)

--- src/main/java/be15fintomatokatchupbe/calendar/query/mapper/ScheduleQueryMapper.java
+++ src/main/java/be15fintomatokatchupbe/calendar/query/mapper/ScheduleQueryMapper.java
@@ -4,13 +4,13 @@ import be15fintomatokatchupbe.calendar.query.dto.ScheduleListsAllDTO;
 import be15fintomatokatchupbe.calendar.query.dto.ScheduleListDTO;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Mapper
 public interface ScheduleQueryMapper {
 
-    List<ScheduleListDTO> getScheduleList(Long userId, LocalDate scheduleDate);
+    List<ScheduleListDTO> getScheduleList(Long userId, LocalTime startTime);
 
     List<ScheduleListsAllDTO> getScheduleListsAll(Long userId);
 }
