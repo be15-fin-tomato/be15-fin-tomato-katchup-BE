3cc6b7311a847422495d930c228a00f47f6448eb
oyk0510 <youkyungoh0510@naver.com>
2025-06-24 15:22:49 +0900

    feat: 고객사 아이디로 사원을 찾는 매퍼 메서드 추가

--- src/main/java/be15fintomatokatchupbe/client/query/mapper/ClientCompanyQueryMapper.java
+++ src/main/java/be15fintomatokatchupbe/client/query/mapper/ClientCompanyQueryMapper.java
@@ -1,6 +1,7 @@
 package be15fintomatokatchupbe.client.query.mapper;
 
 import be15fintomatokatchupbe.client.query.dto.ClientCompanyDetailResponse;
+import be15fintomatokatchupbe.client.query.dto.ClientManagerSimpleResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Mapper
 public interface ClientCompanyQueryMapper {
     ClientCompanyDetailResponse findClientCompanyDetailById(@Param("clientCompanyId") Long clientCompanyId);
+
+    List<ClientManagerSimpleResponse> findManagersByClientCompanyId(@Param("clientCompanyId") Long clientCompanyId);
 }
