b0c49f4f0ce51850cd7ddda33bf3c85614772acf
oyk0510 <youkyungoh0510@naver.com>
2025-06-24 12:02:14 +0900

    feat: 고객사 및 사원 상세 정보 조회 응답 dto 생성

--- src/main/java/be15fintomatokatchupbe/client/query/dto/ClientCompanyDetailResponse.java
+++ src/main/java/be15fintomatokatchupbe/client/query/dto/ClientCompanyDetailResponse.java
@@ -1,4 +1,15 @@
 package be15fintomatokatchupbe.client.query.dto;
 
+import lombok.Data;
+
+@Data   // @toString + @getter + @setter + @RequiredArgsConstructor + @EqualsAndHashCode
 public class ClientCompanyDetailResponse {
+    private Long clientCompanyId;
+    private String name;
+    private Long statusId;
+    private Long managerId;
+    private String managerName;
+    private String managerPosition;
+    private String managerDepartment;
+    private String managerEmail;
 }
