6d1c56c2fb6c2e364fde148bb7d5d4486e5df535
oyk0510 <youkyungoh0510@naver.com>
2025-06-24 12:03:31 +0900

    feat: 고객사 및 사원 상세 조회 서비스 로직 생성

--- src/main/java/be15fintomatokatchupbe/client/query/service/ClientCompanyQueryService.java
+++ src/main/java/be15fintomatokatchupbe/client/query/service/ClientCompanyQueryService.java
@@ -1,4 +1,20 @@
 package be15fintomatokatchupbe.client.query.service;
 
+import be15fintomatokatchupbe.client.query.dto.ClientCompanyDetailResponse;
+import be15fintomatokatchupbe.client.query.mapper.ClientCompanyQueryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ClientCompanyQueryService {
+
+    private final ClientCompanyQueryMapper queryMapper;
+
+    public ClientCompanyDetailResponse getDetail(Long clientCompanyId) {
+        return queryMapper.findClientCompanyDetailById(clientCompanyId)
+                .stream()
+                .findFirst()
+                .orElse(null);
+    }
 }
