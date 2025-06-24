c7c458eb76eb748ff4c5a4cbf6877e556b737c66
oyk0510 <youkyungoh0510@naver.com>
2025-06-24 12:41:20 +0900

    fix: 고객사 조회 필드 추가

--- src/main/java/be15fintomatokatchupbe/client/query/dto/ClientCompanyDetailResponse.java
+++ src/main/java/be15fintomatokatchupbe/client/query/dto/ClientCompanyDetailResponse.java
@@ -2,14 +2,22 @@ package be15fintomatokatchupbe.client.query.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data   // @toString + @getter + @setter + @RequiredArgsConstructor + @EqualsAndHashCode
 public class ClientCompanyDetailResponse {
     private Long clientCompanyId;
-    private String name;
-    private Long statusId;
-    private Long managerId;
-    private String managerName;
-    private String managerPosition;
-    private String managerDepartment;
-    private String managerEmail;
+    private String clientCompanyName;
+    private Long clientCompanyStatusId;
+    private String businessId;
+    private Long sales;
+    private Integer numberOfEmployees;
+    private String telephone;
+    private String fax;
+    private String address;
+    private String detailAddress;
+    private String notes;
+
+    private List<Long> userIds;
+    private List<ClientManagerDTO> clientManagers;
 }
