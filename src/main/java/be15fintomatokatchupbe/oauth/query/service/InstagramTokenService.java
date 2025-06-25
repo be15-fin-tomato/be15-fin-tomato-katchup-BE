bbdd9c77729edb40536f5e9afbeb6f27f25c7210
lddocy <jkcy911219@naver.com>
2025-06-26 01:47:08 +0900

    fix: 안 쓰는 import 삭제

--- src/main/java/be15fintomatokatchupbe/oauth/query/service/InstagramTokenService.java
+++ src/main/java/be15fintomatokatchupbe/oauth/query/service/InstagramTokenService.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
