1bd3f7c10fb69057816302752d55e879291556da
lddocy <jkcy911219@naver.com>
2025-06-25 21:26:52 +0900

    chore: WebClientConfig 인스타 관련 메서드 추가

--- src/main/java/be15fintomatokatchupbe/config/WebClientConfig.java
+++ src/main/java/be15fintomatokatchupbe/config/WebClientConfig.java
@@ -2,6 +2,8 @@ package be15fintomatokatchupbe.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -11,4 +13,13 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder().build();
     }
+
+    @Bean
+    public WebClient facebookWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://graph.facebook.com/v23.0")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
 }
