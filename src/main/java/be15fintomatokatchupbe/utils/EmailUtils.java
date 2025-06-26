a258aaff0cd6e8acd943cfb7e15fa166285d9e97
Joonspar <sdwang15@naver.com>
2025-06-26 10:36:36 +0900

    feat : 메일 전송

--- src/main/java/be15fintomatokatchupbe/utils/EmailUtils.java
+++ src/main/java/be15fintomatokatchupbe/utils/EmailUtils.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -15,18 +18,26 @@ public class EmailUtils {
     private final JavaMailSender javaMailSender;
 
     public void sendEmail(String content, String title, String targetEmail) {
+        sendEmail(content, title, targetEmail, null); // 새로운 메서드로 위임
+    }
+
+    public void sendEmail(String content, String title, String targetEmail, MultipartFile file) {
         MimeMessage message = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper messageHelper = new MimeMessageHelper(
                     message,
-                    true,
+                    true, // multipart
                     "UTF-8"
             );
 
             messageHelper.setTo(targetEmail);
-            messageHelper.setText(content, true);
-//            messageHelper.setFrom("b612");
             messageHelper.setSubject(title);
+            messageHelper.setText(content, true);
+
+            // 파일이 있는 경우 첨부
+            if (file != null && !file.isEmpty()) {
+                messageHelper.addAttachment(file.getOriginalFilename(), file);
+            }
 
             javaMailSender.send(message);
         } catch (MessagingException e) {
