38950c6cdcf9c952042d9a61bd315e19f618e5b4
kkkwid <lsj9057@daum.net>
2025-06-27 09:29:06 +0900

    브랜치 이동

--- src/main/java/be15fintomatokatchupbe/contract/query/controller/ContractSuccessQueryController.java
+++ src/main/java/be15fintomatokatchupbe/contract/query/controller/ContractSuccessQueryController.java
@@ -3,6 +3,7 @@ package be15fintomatokatchupbe.contract.query.controller;
 import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.contract.query.dto.request.ContractSuccessRequest;
 import be15fintomatokatchupbe.contract.query.dto.response.ContractSuccessResponse;
+import be15fintomatokatchupbe.contract.query.dto.response.ContractViewResponse;
 import be15fintomatokatchupbe.contract.query.service.ContractSuccessQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "계약 완료 페이지")
@@ -31,4 +34,16 @@ public class ContractSuccessQueryController {
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
+
+    @Operation(summary = "계약서 조회",description = "사용자는 등록된 계약서를 조회할 수 있다.")
+    @GetMapping("/success/view/{contractId}")
+    public ResponseEntity<ApiResponse<ContractViewResponse>> getContractView (
+            @PathVariable Long contractId,
+            @RequestPart String password
+    ) {
+        ContractViewResponse response = contractSuccessQueryService.getContentView(contractId, password);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
 }
--- src/main/java/be15fintomatokatchupbe/contract/query/service/ContractSuccessQueryService.java
+++ src/main/java/be15fintomatokatchupbe/contract/query/service/ContractSuccessQueryService.java
@@ -1,9 +1,16 @@
 package be15fintomatokatchupbe.contract.query.service;
 
 import be15fintomatokatchupbe.common.dto.Pagination;
+import be15fintomatokatchupbe.common.exception.BusinessException;
+import be15fintomatokatchupbe.contract.command.domain.entity.Contract;
+import be15fintomatokatchupbe.contract.command.domain.entity.ContractFile;
+import be15fintomatokatchupbe.contract.command.domain.repository.ContractFileRepository;
+import be15fintomatokatchupbe.contract.command.domain.repository.ContractRepository;
+import be15fintomatokatchupbe.contract.exception.ContractErrorCode;
 import be15fintomatokatchupbe.contract.query.dto.request.ContractSuccessRequest;
 import be15fintomatokatchupbe.contract.query.dto.response.ContractSuccessDTO;
 import be15fintomatokatchupbe.contract.query.dto.response.ContractSuccessResponse;
+import be15fintomatokatchupbe.contract.query.dto.response.ContractViewResponse;
 import be15fintomatokatchupbe.contract.query.mapper.ContractSuccessMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +22,8 @@ import java.util.List;
 public class ContractSuccessQueryService {
 
     private final ContractSuccessMapper contractSuccessMapper;
+    private final ContractFileRepository contractFileRepository;
+    private final ContractRepository contractRepository;
 
     public ContractSuccessResponse getContractSuccess(ContractSuccessRequest request) {
 
@@ -34,5 +43,25 @@ public class ContractSuccessQueryService {
                         .totalCount(totalList)
                         .build())
                 .build();
+    }
+
+    public ContractViewResponse getContentView(Long contractId, String password) {
+
+        /* 계약서 테이블에서 값 찾기 */
+        Contract contract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new BusinessException(ContractErrorCode.NOT_FOUND));
+
+        /* 계약서 테이블에서 fileId 를 갖고 contractFileId 을 찾아옴 */
+        ContractFile contractFile = contractFileRepository.findById(contract.getFile().getFileId())
+                .orElseThrow(() -> new BusinessException(ContractErrorCode.NON_UPLOAD_FILE));
+
+        /* 비밀번호가 다를 경우 */
+        if(!password.equals(contractFile.getPassword())) {
+            throw new BusinessException(ContractErrorCode.PASSWORD_ERROR);
+        }
+
+
+        return null;
+
     }
 }
