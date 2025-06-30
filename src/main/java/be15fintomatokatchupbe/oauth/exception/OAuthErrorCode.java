afc7d56a86cc3cbae405551836c53cf86b9155ff
lddocy <jkcy911219@naver.com>
2025-06-30 16:05:46 +0900

    feat: OAuth 도메인 에러코드 추가

--- src/main/java/be15fintomatokatchupbe/oauth/exception/OAuthErrorCode.java
+++ src/main/java/be15fintomatokatchupbe/oauth/exception/OAuthErrorCode.java
@@ -11,7 +11,9 @@ import org.springframework.http.HttpStatus;
 public enum OAuthErrorCode implements ErrorCode {
     /* OAuth Error */
     INVALID_OAUTH_CODE("50001", "코드 값이 비어있거나 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
-    INSTAGRAM_STATS_ERROR("50002", "인스타그램 통계 조회 중 오류가 발생했습니다.", HttpStatus.BAD_REQUEST);
+    INSTAGRAM_STATS_ERROR("50002", "인스타그램 통계 조회 중 오류가 발생했습니다.", HttpStatus.BAD_REQUEST),
+    MEDIA_NOT_FOUND("50003", "게시물 조회에 실패 실패했습니다.", HttpStatus.BAD_REQUEST),
+    UNSUPPORTED_MEDIA_TYPE("50004", "지원하지 않는 게시글 형식입니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
