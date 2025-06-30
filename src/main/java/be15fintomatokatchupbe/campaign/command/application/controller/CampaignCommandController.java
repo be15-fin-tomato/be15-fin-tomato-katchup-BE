29012dcde9235e3f79748023699e45882659b452
Jangwoo0710 <jangwoo0710@gmail.com>
2025-06-30 11:38:59 +0900

    TMT 160 be 견적 의견 삭제 기능 구현

--- src/main/java/be15fintomatokatchupbe/campaign/command/application/controller/CampaignCommandController.java
+++ src/main/java/be15fintomatokatchupbe/campaign/command/application/controller/CampaignCommandController.java
@@ -3,13 +3,7 @@ package be15fintomatokatchupbe.campaign.command.application.controller;
 import be15fintomatokatchupbe.campaign.command.application.dto.request.*;
 import be15fintomatokatchupbe.campaign.command.application.service.*;
 import be15fintomatokatchupbe.common.dto.ApiResponse;
-import be15fintomatokatchupbe.common.exception.BusinessException;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
-import be15fintomatokatchupbe.user.command.application.repository.UserRepository;
-import be15fintomatokatchupbe.user.command.application.support.UserHelperService;
-import be15fintomatokatchupbe.user.command.domain.aggregate.User;
-import be15fintomatokatchupbe.user.exception.UserErrorCode;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,12 +24,8 @@ public class CampaignCommandController {
     private final CampaignCommandService campaignCommandService;
     private final ProposalCommandService proposalCommandService;
     private final ContractCommandService contractCommandService;
-    private final ListupCommandService listupCommandService;
     private final QuotationCommandService quotationCommandService;
     private final RevenueCommandService revenueCommandService;
-    private final IdeaCommandService ideaCommandService;
-    private final UserRepository userRepository;
-    private final UserHelperService userHelperService;
 
 
     @PostMapping("/chance/create")
@@ -75,19 +65,6 @@ public class CampaignCommandController {
 
         return ResponseEntity.ok(ApiResponse.success(null));
     }
-
-    // 견적 의견 삭제 기능
-    @Operation(summary = "견적 의견 삭제", description = "사용자는 견적에 작성된 의견을 삭제할 수 있다.(soft delete)")
-    @DeleteMapping("quotation/idea/{ideaId}")
-    public ResponseEntity<ApiResponse<Void>> deleteQuotationIdea(
-            @AuthenticationPrincipal CustomUserDetail customUserDetail,
-            @PathVariable Long ideaId
-    ){
-        User user = userHelperService.findValidUser(customUserDetail.getUserId());
-        ideaCommandService.deleteQuotationIdea(user, ideaId);
-        return ResponseEntity.ok(ApiResponse.success(null));
-    }
-
 
     @PostMapping("/contract/create")
     public ResponseEntity<ApiResponse<Void>> createContract(
