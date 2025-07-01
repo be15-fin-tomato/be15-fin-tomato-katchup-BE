2e9eec9ade4ec40a1032f0ec03f01e7c38da3a33
Jangwoo0710 <jangwoo0710@gmail.com>
2025-07-01 10:34:12 +0900

    TMT 169 be 파이프라인 의견 전체 조회 기능 구현

---
+++ src/main/java/be15fintomatokatchupbe/campaign/query/dto/mapper/IdeaDTO.java
@@ -0,0 +1,20 @@
+package be15fintomatokatchupbe.campaign.query.dto.mapper;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+public class IdeaDTO {
+
+    private Long ideaId;
+    private Long pipelineId;
+    private Long userId;
+    private String content;
+    private LocalDateTime createdAt;
+
+}
