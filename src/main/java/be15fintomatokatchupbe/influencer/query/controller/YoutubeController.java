f4fa75894f02bd474853d4c20f322905beb92758
lddocy <jkcy911219@naver.com>
2025-07-03 17:20:08 +0900

    chore: 인플루언서 도메인 스웨거 작성

--- src/main/java/be15fintomatokatchupbe/influencer/query/controller/YoutubeController.java
+++ src/main/java/be15fintomatokatchupbe/influencer/query/controller/YoutubeController.java
@@ -3,6 +3,8 @@ package be15fintomatokatchupbe.influencer.query.controller;
 import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.dashboard.query.service.SearchRatioQueryService;
 import be15fintomatokatchupbe.influencer.query.service.YoutubeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "댓글 요약")
 @Slf4j
 @RestController
 @RequestMapping("/youtube")
@@ -19,6 +22,7 @@ public class YoutubeController {
     private final SearchRatioQueryService searchRatioQueryService;
     private final YoutubeService youtubeService;
 
+    @Operation(summary = "유튜브 댓글 요약 ", description = "사용자는 등록된 유튜브 영상의 댓글 목록을 조회할 수 있다.")
     @GetMapping("/commentssummary/{campaignId}")
     public ResponseEntity<ApiResponse<List<String>>> getCommentsByCampaignId(
             @PathVariable Long campaignId
