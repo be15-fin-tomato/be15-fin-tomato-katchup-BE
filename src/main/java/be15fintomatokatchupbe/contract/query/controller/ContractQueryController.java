00d4e37ea00bdb4165de194ec7c29dc028f95b37
lddocy <jkcy911219@naver.com>
2025-07-03 17:12:56 +0900

    chore: 계약서 도메인 스웨거 작성

--- src/main/java/be15fintomatokatchupbe/contract/query/controller/ContractQueryController.java
+++ src/main/java/be15fintomatokatchupbe/contract/query/controller/ContractQueryController.java
@@ -4,12 +4,15 @@ import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.contract.query.dto.response.ContractPageResponse;
 import be15fintomatokatchupbe.contract.query.dto.response.ObjectResponse;
 import be15fintomatokatchupbe.contract.query.service.ContractQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "계약서 조회")
 @RestController
 @RequestMapping("/contracts")
 @RequiredArgsConstructor
@@ -17,12 +20,14 @@ public class ContractQueryController {
 
     private final ContractQueryService contractQueryService;
 
+    @Operation(summary = "계약서 조건 목록 조회", description = "사용자는 계약서 목적의 목록을 조회할 수 있다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<ObjectResponse>>> getAllObjects() {
         List<ObjectResponse> objects = contractQueryService.getAllObjects();
         return ResponseEntity.ok(ApiResponse.success(objects));
     }
 
+    @Operation(summary = "계약서 조건별 상세 조회", description = "사용자는 계약서를 조건별로 상세 조회할 수 있다.")
     @GetMapping("/{objectId}")
     public ResponseEntity<ApiResponse<ContractPageResponse>> getContractPage(
             @PathVariable Long objectId,
--- src/main/java/be15fintomatokatchupbe/contract/query/controller/SendEmailController.java
+++ src/main/java/be15fintomatokatchupbe/contract/query/controller/SendEmailController.java
@@ -3,6 +3,8 @@ package be15fintomatokatchupbe.contract.query.controller;
 import be15fintomatokatchupbe.contract.query.dto.request.ClientManagerSearchRequest;
 import be15fintomatokatchupbe.contract.query.dto.response.ClientManagerResponse;
 import be15fintomatokatchupbe.contract.query.service.SendEmailService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "이메일 전송")
 @RestController
 @RequestMapping("/emails")
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class SendEmailController {
 
     private final SendEmailService sendEmailService;
 
+    @Operation(summary = "이메일 전송", description = "사용자는 서비스 내에서 담당자에게 이메일을 전송할 수 있다.")
     @GetMapping("/recipients")
     public ResponseEntity<List<ClientManagerResponse>> getRecipients(
             @ModelAttribute ClientManagerSearchRequest request) {
