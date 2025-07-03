d0c42df36b4dde40d0e5c69b09e7711a2eadfb6e
oyk0510 <youkyungoh0510@naver.com>
2025-07-03 15:19:44 +0900

    feat: 파이프라인 단계 dto 응답 생성

---
+++ src/main/java/be15fintomatokatchupbe/campaign/query/dto/response/PipelineStepStatusDto.java
@@ -0,0 +1,11 @@
+package be15fintomatokatchupbe.campaign.query.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PipelineStepStatusDto {
+    private String stepType;
+    private String startedAt;
+}
