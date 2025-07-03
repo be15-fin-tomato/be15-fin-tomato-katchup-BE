8a2240ffbcdfac77d01556b22f8302150653a80b
lddocy <jkcy911219@naver.com>
2025-07-03 17:23:26 +0900

    chore: 알림 도메인 스웨거 작성

--- src/main/java/be15fintomatokatchupbe/notification/command/application/controller/FcmController.java
+++ src/main/java/be15fintomatokatchupbe/notification/command/application/controller/FcmController.java
@@ -4,6 +4,8 @@ import be15fintomatokatchupbe.common.dto.ApiResponse;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
 import be15fintomatokatchupbe.notification.command.application.dto.request.FcmTokenRequest;
 import be15fintomatokatchupbe.notification.command.application.service.FcmService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "fcm 알림")
 @RestController
 @RequestMapping("/fcm/token")
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ public class FcmController {
     private final FcmService fcmService;
 
     @PostMapping
+    @Operation(summary = "FCM 토큰 등록", description = "로그인한 사용자의 FCM 토큰을 서버에 등록합니다.")
     public ResponseEntity<ApiResponse<Void>> getFcmToken(
             @AuthenticationPrincipal CustomUserDetail userDetail,
             @RequestBody FcmTokenRequest fcmTokenRequest
