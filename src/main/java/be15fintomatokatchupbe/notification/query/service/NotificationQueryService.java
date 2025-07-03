2bd9fa85f5cd81a398a4e1c664c64f97fd6110a2
Jangwoo0710 <jangwoo0710@gmail.com>
2025-07-03 15:36:05 +0900

    TMT 181 be 읽지 않은 알림 수 조회 기능 구현

--- src/main/java/be15fintomatokatchupbe/notification/query/service/NotificationQueryService.java
+++ src/main/java/be15fintomatokatchupbe/notification/query/service/NotificationQueryService.java
@@ -1,5 +1,7 @@
 package be15fintomatokatchupbe.notification.query.service;
 
+import be15fintomatokatchupbe.common.domain.StatusType;
+import be15fintomatokatchupbe.notification.command.domain.repository.NotificationRepository;
 import be15fintomatokatchupbe.notification.query.dto.NotificationsAllDTO;
 import be15fintomatokatchupbe.notification.query.dto.response.NotificationsAllResponse;
 import be15fintomatokatchupbe.notification.query.mapper.NotificationQueryMapper;
@@ -13,11 +15,18 @@ import java.util.List;
 public class NotificationQueryService {
 
     private final NotificationQueryMapper notificationQueryMapper;
+    private final NotificationRepository notificationRepository;
 
     public NotificationsAllResponse getNotificationsAll(Long userId) {
         List<NotificationsAllDTO> notificationsAll = notificationQueryMapper.getNotificationsAll(userId);
         return NotificationsAllResponse.builder()
                 .notifications(notificationsAll)
                 .build();
+    }
+
+    public long countUnreadNotifications(Long userId) {
+        return notificationRepository.countByUserIdAndIsReadAndIsDeleted(
+                userId, StatusType.N, StatusType.N
+        );
     }
 }
