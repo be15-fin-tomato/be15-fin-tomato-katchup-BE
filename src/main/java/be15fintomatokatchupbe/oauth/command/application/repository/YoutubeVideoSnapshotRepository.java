74b2a3730529709ce0cfde7ae15c04d29c088b7e
lddocy <jkcy911219@naver.com>
2025-07-08 23:48:00 +0900

    feat: 유튜브 계정 인기 동영상 저장 레포지토리 생성

---
+++ src/main/java/be15fintomatokatchupbe/oauth/command/application/repository/YoutubeVideoSnapshotRepository.java
@@ -0,0 +1,15 @@
+package be15fintomatokatchupbe.oauth.command.application.repository;
+
+import be15fintomatokatchupbe.oauth.command.application.domain.YoutubeVideoSnapshot;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface YoutubeVideoSnapshotRepository extends JpaRepository<YoutubeVideoSnapshot, Long> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM YoutubeVideoSnapshot v WHERE v.snapshot.id = :snapshotId")
+    void deleteBySnapshotId(@Param("snapshotId") Long snapshotId);
+}
