75a10b923b00062241ce1490a2887e4947a14756
lddocy <jkcy911219@naver.com>
2025-07-09 01:14:39 +0900

    feat: 인플루언서 개별 조회 기능 구현

--- src/main/java/be15fintomatokatchupbe/influencer/query/mapper/InfluencerMapper.java
+++ src/main/java/be15fintomatokatchupbe/influencer/query/mapper/InfluencerMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface InfluencerMapper {
@@ -16,5 +17,8 @@ public interface InfluencerMapper {
     List<InfluencerSearchDto> findInfluencerList(String keyword);
 
     List<CategoryDto> findCategoryList();
+
+    Optional<InfluencerCardResponse> findInfluencerById(@Param("influencerId") Long influencerId);
+
 }
 
