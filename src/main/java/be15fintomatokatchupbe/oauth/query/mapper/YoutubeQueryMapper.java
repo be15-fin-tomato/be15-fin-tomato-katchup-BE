c5d9c0d07b585a5d4c85fef534ab93fbbb0918b6
lddocy <jkcy911219@naver.com>
2025-07-10 16:19:31 +0900

    feat: 유튜브 인플루언서 인기 동영상 조회 쿼리 추가

--- src/main/java/be15fintomatokatchupbe/oauth/query/mapper/YoutubeQueryMapper.java
+++ src/main/java/be15fintomatokatchupbe/oauth/query/mapper/YoutubeQueryMapper.java
@@ -1,6 +1,7 @@
 package be15fintomatokatchupbe.oauth.query.mapper;
 
 import be15fintomatokatchupbe.oauth.command.application.domain.YoutubeStatsSnapshot;
+import be15fintomatokatchupbe.oauth.query.dto.YoutubeVideoInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 @Mapper
 public interface YoutubeQueryMapper {
     List<YoutubeStatsSnapshot> findYoutubeStats(Long influencerId);
+    List<YoutubeVideoInfo> findTopVideos(Long influencerId);
 }
 
