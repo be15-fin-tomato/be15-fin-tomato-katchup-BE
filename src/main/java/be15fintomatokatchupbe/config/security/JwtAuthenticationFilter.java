865df1457c08394a2ba24bd0f8cd110adcca0ebd
Cho-Hyun-Seung <toki0327@naver.com>
2025-07-13 03:30:16 +0900

    fix: AT 에러 코드 응답 500 -> 401 수정

--- src/main/java/be15fintomatokatchupbe/config/security/JwtAuthenticationFilter.java
+++ src/main/java/be15fintomatokatchupbe/config/security/JwtAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package be15fintomatokatchupbe.config.security;
 
+import be15fintomatokatchupbe.common.exception.BusinessException;
+import be15fintomatokatchupbe.common.exception.GlobalErrorCode;
 import be15fintomatokatchupbe.config.security.model.CustomUserDetail;
+import be15fintomatokatchupbe.utils.jwt.JwtErrorResponse;
 import be15fintomatokatchupbe.utils.jwt.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,35 +24,41 @@ import java.util.Arrays;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtErrorResponse jwtErrorResponse;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String upgradeHeader = request.getHeader("Upgrade");
+            if (upgradeHeader != null && "websocket".equalsIgnoreCase(upgradeHeader)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = parseToken(request);
 
-        String upgradeHeader = request.getHeader("Upgrade");
-        if (upgradeHeader != null && "websocket".equalsIgnoreCase(upgradeHeader)) {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Long userId = jwtTokenProvider.getUserIdFromJWT(token);
+                String loginId = jwtTokenProvider.getLoginIdFromJWT(token);
+
+                CustomUserDetail userDetail = CustomUserDetail.builder()
+                        .userId(userId)
+                        .loginId(loginId)
+                        .build();
+                PreAuthenticatedAuthenticationToken authentication
+                        = new PreAuthenticatedAuthenticationToken(
+                        userDetail, null, userDetail.getAuthorities()
+                );
+                log.info("[Jwt Filter] 인증된 유저: userId={}, loginId={}", userId, loginId);
+
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
             filterChain.doFilter(request, response);
-            return;
+        } catch (BusinessException e){
+            jwtErrorResponse.setErrorResponse(response, e.getErrorCode());
+        } catch (Exception e){
+            jwtErrorResponse.setErrorResponse(response, GlobalErrorCode.INTERNAL_SERVER_ERROR);
         }
-        String token = parseToken(request);
-
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Long userId = jwtTokenProvider.getUserIdFromJWT(token);
-            String loginId = jwtTokenProvider.getLoginIdFromJWT(token);
-
-            CustomUserDetail userDetail = CustomUserDetail.builder()
-                    .userId(userId)
-                    .loginId(loginId)
-                    .build();
-            PreAuthenticatedAuthenticationToken authentication
-                    = new PreAuthenticatedAuthenticationToken(
-                    userDetail, null, userDetail.getAuthorities()
-            );
-            log.info("[Jwt Filter] 인증된 유저: userId={}, loginId={}", userId, loginId);
-
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-        filterChain.doFilter(request, response);
     }
 
     private String parseToken(HttpServletRequest request) {
