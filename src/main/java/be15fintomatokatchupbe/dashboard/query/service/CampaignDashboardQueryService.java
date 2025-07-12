ab36ce2f3f1503bbe208500241be9f13efce79df
Jangwoo0710 <jangwoo0710@gmail.com>
2025-07-12 21:49:46 +0900

    TMT 281 be 캠페인 컨텐츠 정보 조회 수정

--- src/main/java/be15fintomatokatchupbe/dashboard/query/service/CampaignDashboardQueryService.java
+++ src/main/java/be15fintomatokatchupbe/dashboard/query/service/CampaignDashboardQueryService.java
@@ -23,30 +23,6 @@ public class CampaignDashboardQueryService {
     private final CampaignDashboardQueryMapper mapper;
     private final YoutubeService youtubeService;
 
-//    public CampaignContentResponse getCampaignContent(Long pipelineInfluencerId) {
-//        // 1. pipelineId 조회
-//        Long pipelineId = mapper.findPipelineIdByPipelineInfluencerId(pipelineInfluencerId);
-//        if (pipelineId == null) {
-//            throw new BusinessException(CampaignErrorCode.PIPELINE_STATUS_NOT_FOUND);
-//        }
-//
-//        // 2. 유튜브 링크 조회
-//        String youtubeLink = mapper.findYoutubeLinkByPipelineInfluencerId(pipelineInfluencerId);
-//
-//        if (youtubeLink == null || youtubeLink.isEmpty()) {
-//            throw new BusinessException(CampaignErrorCode.INVALID_YOUTUBE_LINK);
-//        }
-//
-//        // 3. videoId 추출
-//        String videoId = YoutubeService.extractVideoId(youtubeLink);
-//
-//        // 4. 유튜브 메트릭 조회
-//        Map<String, Long> metrics = youtubeService.getVideoMetrics(videoId);
-//
-//        // 5. 응답 객체 생성
-//        return new CampaignContentResponse(metrics);
-//    }
-
     public CampaignContentResponse getCampaignContent(Long pipelineInfluencerId) {
         Long pipelineId = mapper.findPipelineIdByPipelineInfluencerId(pipelineInfluencerId);
         log.info("pipelineId: {}", pipelineId);
@@ -68,7 +44,6 @@ public class CampaignDashboardQueryService {
 
         return new CampaignContentResponse(metrics);
     }
-
 
     @Transactional // 트랜잭션 경고 해결을 위해 추가
     public CampaignGetRevenueResponse getRevenue(Long pipelineInfluencerId) { // <-- 파라미터 변경
